af7c5ab849d8fc035b65eaa9df6b4437054bf088
Alexander Shelepenok <alxshelepenok@gmail.com>
2023-09-07 14:42:25 +0000

    chore: fix nexus registry

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 IMAGE_NAME="alxshelepenok/grafana"
-NEXUS_REGISTRY="docker-hosted.nexus.infrastructure.alxshelepenok"
+NEXUS_REGISTRY="docker-hosted.nexus.infrastructure.alxshelepenok.com"
 TAG="${1}"
 docker build -t ${IMAGE_NAME}:"${TAG}" .
 docker tag ${IMAGE_NAME}:"${TAG}" ${NEXUS_REGISTRY}/${IMAGE_NAME}:"${TAG}"
